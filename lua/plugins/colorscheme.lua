1a807e4372ce7425316a0180e323fa623e507bb7
Anar K. Jafarov <anar.k.jafarov@gmail.com>
2024-07-19 01:34:01 +0100

    20240719-001

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "joshdick/onedark.vim",
+    priority = 1000, -- Ensure it loads first
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
