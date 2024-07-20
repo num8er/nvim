92c9e96bb168e6b53786dadb9d61822b53f2b60d
Anar K. Jafarov <anar.k.jafarov@gmail.com>
2024-07-20 20:36:14 +0100

    switched to tokyonight to support transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,28 @@
 return {
+  -- disabling cause does not support transparency
+  --  {
+  --    "joshdick/onedark.vim",
+  --    priority = 1000, -- Ensure it loads first
+  --  },
   {
-    "joshdick/onedark.vim",
-    priority = 1000, -- Ensure it loads first
+    "tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
   },
-
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "tokyonight",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
 }
