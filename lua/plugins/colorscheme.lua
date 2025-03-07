8bdded2f019ba86bbbfcc95f945e6e43968c71f0
Anar K. Jafarov <anar.k.jafarov@gmail.com>
2025-03-07 14:01:13 +0000

    solarized dark with transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,10 +3,23 @@ return {
   lazy = false,
   priority = 1000,
   ---@type solarized.config
-  opts = {},
+  opts = {
+    transparent = {
+      enabled = true, -- Master switch to enable transparency
+      pmenu = true, -- Popup menu (e.g., autocomplete suggestions)
+      normal = true, -- Main editor window background
+      normalfloat = true, -- Floating windows
+      neotree = true, -- Neo-tree file explorer
+      nvimtree = true, -- Nvim-tree file explorer
+      whichkey = true, -- Which-key popup
+      telescope = true, -- Telescope fuzzy finder
+      lazy = true, -- Lazy plugin manager UI
+      mason = true, -- Mason manage external tooling
+    },
+  },
   config = function(_, opts)
     vim.o.termguicolors = true
-    vim.o.background = "light"
+    vim.o.background = "dark"
     require("solarized").setup(opts)
     vim.cmd.colorscheme("solarized")
   end,
