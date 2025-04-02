dc13176732acb9f7bb1f49602ea37f0068f0840b
Anar K. Jafarov <anar.k.jafarov@gmail.com>
2025-04-02 21:26:53 +0100

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,15 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_blink_main = false
+
+vim.diagnostic.config({
+    virtual_lines = false,
+    virtual_text = false,
+    signs = true,
+    float = {
+        source = 'always',
+        show_header = true,
+    },
+    update_in_insert = false,
+    severity_sort = false, 
+})
