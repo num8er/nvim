ad6586fb0b897b94f3f35da160380cf9a3572f03
Anar Jafarov <anar.k.jafarov@gmail.com>
2025-06-14 15:20:36 +0100

    fix js/ts debug support using DAP
    Signed-off-by: Anar Jafarov <anar.k.jafarov@gmail.com>

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
 require("config/lspconfig")
+require('config/dap')
--- lua/config/dap.lua
+++ lua/config/dap.lua
@@ -1,10 +1,57 @@
-local dap = require('dap')
+local dap_ok, dap = pcall(require, "dap")
+local dap_ui_ok, ui = pcall(require, "dapui")
 
 dap.adapters.lldb = {
   type = 'executable',
   command = '/usr/bin/lldb',
   name = 'lldb'
 }
+
+if not (dap_ok and dap_ui_ok) then
+  require("notify")("dap-ui not installed!", "warning")
+  return
+end
+
+ui.setup({
+  icons = { expanded = "▾", collapsed = "▸" },
+  mappings = {
+    open = "o",
+    remove = "d",
+    edit = "e",
+    repl = "r",
+    toggle = "t",
+  },
+  expand_lines = vim.fn.has("nvim-0.7"),
+  layouts = {
+    {
+      elements = {
+        "scopes",
+      },
+      size = 0.3,
+      position = "right"
+    },
+    {
+      elements = {
+        "repl",
+        "breakpoints"
+      },
+      size = 0.3,
+      position = "bottom",
+    },
+  },
+  floating = {
+    max_height = nil,
+    max_width = nil,
+    border = "single",
+    mappings = {
+      close = { "q", "<Esc>" },
+    },
+  },
+  windows = { indent = 1 },
+  render = {
+    max_type_length = nil,
+  },
+})
 
 dap.configurations.zig = {
   {
@@ -34,16 +81,10 @@ dap.configurations.zig = {
   }
 }
 
-dap.configurations.node2 = {
-  type = 'executable',
-  command = 'node',
-  args = { os.getenv('HOME') .. '/.local/share/nvim/mason/packages/node-debug2-adapter/out/src/nodeDebug.js' },
-}
-
 dap.configurations.typescript = {
   {
     name = 'Attach to Docker service',
-    type = 'node2',
+    type = 'node',
     request = 'attach',
     port = 9229,
     cwd = vim.fn.getcwd(),
@@ -54,7 +95,7 @@ dap.configurations.typescript = {
   },
   {
     name = 'Attach to Local service',
-    type = 'node2',
+    type = 'node',
     request = 'attach',
     port = 9229,
     cwd = vim.fn.getcwd(),
@@ -65,3 +106,30 @@ dap.configurations.typescript = {
 }
 
 dap.configurations.javascript = dap.configurations.typescript
+
+vim.fn.sign_define('DapBreakpoint', { text = '🐞' })
+
+vim.keymap.set("n", "<localleader>ds", function()
+  dap.continue()
+  ui.toggle({})
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false) -- Spaces buffers evenly
+end)
+
+vim.keymap.set("n", "<localleader>dl", require("dap.ui.widgets").hover)
+vim.keymap.set("n", "<localleader>dc", dap.continue)
+vim.keymap.set("n", "<localleader>db", dap.toggle_breakpoint)
+vim.keymap.set("n", "<localleader>dn", dap.step_over)
+vim.keymap.set("n", "<localleader>di", dap.step_into)
+vim.keymap.set("n", "<localleader>do", dap.step_out)
+vim.keymap.set("n", "<localleader>dC", function()
+  dap.clear_breakpoints()
+  require("notify")("Breakpoints cleared", "warn")
+end)
+
+vim.keymap.set("n", "<localleader>de", function()
+  dap.clear_breakpoints()
+  ui.toggle({})
+  dap.terminate()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false)
+  require("notify")("Debugger session ended", "warn")
+end)
