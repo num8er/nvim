df956ecbd374308548ceac4e3d62b0a4ba7c745e
Anar K. Jafarov <anar.k.jafarov@gmail.com>
2025-09-14 13:59:53 +0100

    blink auto-complition, updates
    Signed-off-by: Anar K. Jafarov <anar.k.jafarov@gmail.com>

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,7 +1,12 @@
 return {
   "saghen/blink.cmp",
+  -- enabled = false,
+
   -- optional: provides snippets for the snippet source
-  dependencies = { "rafamadriz/friendly-snippets" },
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    "fang2hou/blink-copilot",
+  },
 
   -- use a release tag to download pre-built binaries
   version = "1.*",
@@ -23,9 +28,9 @@ return {
     -- C-n/C-p or Up/Down: Select next/previous item
     -- C-e: Hide menu
     -- C-k: Toggle signature help (if signature.enabled = true)
-    --
+
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { 
+    keymap = {
       preset = "none",
       ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
       ["<CR>"] = { "accept", "fallback" },
@@ -71,7 +76,23 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
+      default = { "lsp", "path", "snippets", "buffer", "copilot" },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-copilot",
+          score_offset = 100,
+          async = true,
+          opts = {
+            max_completitions = 3,
+            max_attempts = 4,
+            kind_name = "Copilot",
+            kind_icon = "@ ",
+            debounce = 200,
+            auto_refres = { backward = true, forward = true }
+          }
+        }
+      }
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
